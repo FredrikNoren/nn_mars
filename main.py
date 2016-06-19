44ed1d8fcc218230dcda7be8ed4efccdc95aba67
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-06-19 15:51:51 -0400

    1 layer convnet + encoding

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import gdalconst
 import matplotlib.pyplot as plt
 import random
 from skimage.measure import block_reduce
+import math
 
 def draw_mars_data(data):
     u = np.unique(data.flatten())
@@ -49,10 +50,13 @@ input_image_size = 16
 show_test_count = 7
 
 # Network Parameters
+n_filters = 16
+filter_size = 3
 n_input = input_image_size * input_image_size
 n_output = image_size * image_size
 n_hidden = 32 # 1st layer num features
 n_hidden_encoding = 32 # 2nd layer num features
+
 
 
 def downsample_block_mean(image, size):
@@ -98,113 +102,81 @@ def get_data_batch(count):
         batch_full.append(full)
     return batch_xs, batch_ys, batch_full
 
-conv_h = tf.Variable(tf.truncated_normal([7, 7, 1, 32], stddev=0.1))
-conv_b = tf.Variable(tf.constant(0.0, shape=[32]))
+conv_h = tf.Variable(tf.truncated_normal([filter_size, filter_size, 1, n_filters], stddev=0.1))
+conv_b = tf.Variable(tf.constant(0.0, shape=[n_filters]))
 def encoder(x):
     x_image = tf.reshape(x, [-1,image_size,image_size,1])
-    return tf.nn.relu(tf.nn.conv2d(x_image, conv_h, strides=[1, 1, 1, 1], padding='SAME') + conv_b)
-    # x_image = tf.reshape(x, [-1,image_size,image_size,1])
-    # conv_layer = tf.nn.relu(tf.nn.conv2d(x_image, conv_h, strides=[1, 1, 1, 1], padding='SAME') + conv_b)
-    # conv_layer_reshape = tf.reshape(conv_layer, [-1, image_size * image_size * 32])
-    # return tf.nn.sigmoid(tf.add(tf.matmul(conv_layer_reshape, tf.Variable(tf.truncated_normal([image_size * image_size * 32, n_hidden_encoding], stddev=0.1))),
-    #                                 tf.Variable(tf.constant(0.1, shape=[n_hidden_encoding]))))
+    conv_l = tf.nn.relu(tf.nn.conv2d(x_image, conv_h, strides=[1, 1, 1, 1], padding='SAME') + conv_b)
+    conv_l_reshape = tf.reshape(conv_l, [-1, image_size * image_size * n_filters])
+    enc_l = tf.nn.tanh(tf.add(tf.matmul(conv_l_reshape, tf.Variable(tf.truncated_normal([image_size * image_size * n_filters, n_hidden_encoding], stddev=0.1))),
+                                     tf.Variable(tf.constant(0.1, shape=[n_hidden_encoding]))))
+    return enc_l, conv_l
 
-
-    # layer_1_flat = tf.reshape(layer_1, [-1, 32*image_size*image_size])
-    # return tf.nn.sigmoid(tf.add(tf.matmul(layer_1_flat, tf.Variable(tf.random_normal([32*image_size*image_size, n_hidden_encoding]))),
-    #                               tf.Variable(tf.random_normal([n_hidden_encoding]))))
-
-    # layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, tf.Variable(tf.random_normal([n_input, n_hidden]))),
-    #                                 tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                                tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                                tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                            tf.Variable(tf.random_normal([n_hidden]))))
-    # return tf.nn.sigmoid(tf.add(tf.matmul(layer_4, tf.Variable(tf.random_normal([n_hidden, n_hidden_encoding]))),
-    #                               tf.Variable(tf.random_normal([n_hidden_encoding]))))
-
-
-# dec_h_1 = tf.Variable(tf.truncated_normal([n_hidden_encoding, 7 * 7 * 32], stddev=0.1))
+dec_h_1 = tf.Variable(tf.truncated_normal([n_hidden_encoding, image_size * image_size * n_filters], stddev=0.1))
+dec_b_1 = tf.Variable(tf.constant(0.1, shape=[image_size * image_size * n_filters]))
 deconv_b = tf.Variable(tf.constant(0.0, shape=[image_size * image_size]))
 #
 # dec_h_2 = tf.Variable(tf.truncated_normal([image_size * image_size * 32, n_output], stddev=0.1))
 # dec_b_2 = tf.Variable(tf.constant(0.1, shape=[n_output]))
 def decoder(x):
     print(x.get_shape())
-    deconv_l = tf.nn.conv2d_transpose(x, conv_h, tf.pack([batch_size, image_size, image_size, 1]), strides=[1, 1, 1, 1], padding='SAME')
+    l_0 = tf.nn.relu(tf.add(tf.matmul(x, dec_h_1), dec_b_1))
+    l_0_reshape = tf.reshape(l_0, [-1, image_size, image_size, n_filters])
+    print(l_0_reshape.get_shape())
+    # shape = (?/256, 16, 16, 32)
+    deconv_l = tf.nn.conv2d_transpose(l_0_reshape, conv_h, tf.pack([batch_size, image_size, image_size, 1]), strides=[1, 1, 1, 1], padding='SAME')
     print(deconv_l.get_shape())
     deconv_l_reshape = tf.reshape(deconv_l, [-1, image_size * image_size])
     print(deconv_l_reshape.get_shape())
     l_1 = tf.nn.relu(deconv_l_reshape + deconv_b)
     print(l_1.get_shape())
 
-    return l_1 #tf.nn.sigmoid(tf.add(tf.matmul(l_1_reshape, dec_h_2), dec_b_2))
-    # layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, dec_h_1), dec_b_1))
-    # layer_1_image = tf.reshape(layer_1, [-1, 7,  7, 32])
-    # deconv_layer = tf.nn.relu(tf.nn.conv2d_transpose(layer_1_image, conv_h, tf.pack([1024, 1, 1, 1]), strides=[1, 1, 1, 1], padding='VALID') + conv_b)
-    # deconv_layer_reshape = tf.reshape(deconv_layer, [-1, 1024 * 1])
-    # return tf.nn.sigmoid(tf.add(tf.matmul(deconv_layer_reshape, dec_h_2), dec_b_2))
-
-    # layer_1 = tf.nn.conv2d(x, tf.Variable(tf.truncated_normal([7, 7, 1, 32])), strides=[1, 1, 1, 1], padding='SAME')
-    # layer_1_flat = tf.reshape(layer_1, [-1, 32*image_size*image_size])
-    # return tf.nn.sigmoid(tf.add(tf.matmul(layer_1_flat, tf.Variable(tf.random_normal([32*image_size*image_size, n_output]))),
-    #                               tf.Variable(tf.random_normal([n_hidden_encoding]))))
-
-    # layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, tf.Variable(tf.random_normal([n_hidden_encoding, n_hidden]))),
-    #                                tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                                tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                            tf.Variable(tf.random_normal([n_hidden]))))
-    # layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, tf.Variable(tf.random_normal([n_hidden, n_hidden]))),
-    #                            tf.Variable(tf.random_normal([n_hidden]))))
-    # return tf.nn.sigmoid(tf.add(tf.matmul(layer_4, tf.Variable(tf.random_normal([n_hidden, n_output]))),
-    #                               tf.Variable(tf.random_normal([n_output]))))
+    return l_1
 
 test_set_xs, test_set_ys, test_set_full = get_data_batch(batch_size)
 
 
-# disp_encoding_placeholder = tf.placeholder("float", [None, n_hidden_encoding])
-# disp_decoder = decoder(disp_encoding_placeholder)
-# disp_rand_encoding = []
-# tmp_rand = np.random.rand(n_hidden_encoding)
-# for i in range(show_test_count):
-#     r = np.copy(tmp_rand)
-#     r[0] = i / show_test_count
-#     disp_rand_encoding.append(r)
-def show_test_set(sess):
-    # Applying encode and decode over test set
-    encode_decode = sess.run(y_pred, feed_dict={X: test_set_xs, Y: test_set_ys})
-    # decoder_random_res = sess.run(disp_decoder, feed_dict={disp_encoding_placeholder: disp_rand_encoding })
+disp_encoding_placeholder = tf.placeholder("float", [None, n_hidden_encoding])
+disp_decoder = decoder(disp_encoding_placeholder)
+disp_rand_encoding = []
+tmp_rand = np.random.rand(n_hidden_encoding)
+for i in range(batch_size):
+    r = np.copy(tmp_rand)
+    r[0] = i / show_test_count
+    disp_rand_encoding.append(r)
+def show_test_set(sess, epoch):
+    encode_decode, conv_l_out = sess.run([y_pred, conv_l], feed_dict={X: test_set_xs, Y: test_set_ys})
+    decoder_random_res = sess.run(disp_decoder, feed_dict={disp_encoding_placeholder: disp_rand_encoding })
+    show_filters(conv_l_out, epoch)
 
     for i in range(show_test_count):
         test_viz_a[0][i].imshow(np.reshape(test_set_xs[i], (input_image_size, input_image_size)), interpolation="nearest")
         test_viz_a[1][i].imshow(np.reshape(test_set_ys[i], (image_size, image_size)), interpolation="nearest")
         test_viz_a[2][i].imshow(np.reshape(encode_decode[i], (image_size, image_size)), interpolation="nearest")
         test_viz_a[3][i].imshow(np.reshape(encode_decode[i] - test_set_ys[i] , (image_size, image_size)), interpolation="nearest")
-        # test_viz_a[4][i].imshow(np.reshape(decoder_random_res[i], (image_size, image_size)), interpolation="nearest")
+        test_viz_a[4][i].imshow(np.reshape(decoder_random_res[i], (image_size, image_size)), interpolation="nearest")
         # x = np.reshape(test_set_xs[i], (int(image_size/2), image_size))
         # y = np.reshape(encode_decode[i], (int(image_size/2), image_size))
         # a[1][i].imshow(np.concatenate((x, y)))
     plt.draw()
 
-filters_plot_f, filters_plot_a = plt.subplots(8, 4, figsize=(8, 4))
+n_filters_sq = int(math.ceil(math.sqrt(n_filters)))
+filters_plot_f, filters_plot_a = plt.subplots(n_filters_sq, n_filters_sq, figsize=(n_filters_sq, n_filters_sq))
 filters_plot_f.show()
-def show_filters():
-    filters = conv_h.eval()
-    for y in range(8):
-        for x in range(4):
-            filters_plot_a[y][x].imshow(filters[0:7,0:7,0,y*4 + x], interpolation="nearest")
-
+def show_filters(data, epoch):
+    print("filters", data.shape)
+    for y in range(n_filters_sq):
+        for x in range(n_filters_sq):
+            i = y*n_filters_sq + x
+            if i < n_filters:
+                filters_plot_a[y][x].imshow(data[0, 0:image_size, 0:image_size, i], interpolation="nearest")
 
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, n_input])
 Y = tf.placeholder("float", [None, n_output])
 
 # Construct model
-encoder_op = encoder(X)
+encoder_op, conv_l = encoder(X)
 decoder_op = decoder(encoder_op)
 
 # Prediction
@@ -254,8 +226,8 @@ for epoch in range(training_epochs):
     if epoch % display_step == 0:
         learn_viz_a.plot([epoch - 1, epoch], [plot_prev, c])
         plot_prev = c
-        show_test_set(sess)
-        show_filters()
+        show_test_set(sess, epoch)
+        #show_filters(conv_h.eval())
         plt.pause(0.05)
         print("Epoch:", '%04d' % (epoch+1),
               "cost=", "{:.9f}".format(c))
