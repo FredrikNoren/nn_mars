11f17e3aba5be6c75a58b04bd78e2d903c84b0c0
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-07-06 21:44:41 -0400

    pnoise and working 64x64

--- dcgan.py
+++ dcgan.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from skimage.measure import block_reduce
+from noise import pnoise2
+
 
 def draw_mars_data(data):
     u = np.unique(data.flatten())
@@ -23,13 +25,14 @@ mars_data = np.load("hirise_20.npy")
 # exit()
 print("Loading mars data done.")
 
-batch_size = 6
+batch_size = 16
 z_dim = 100
 learning_rate_desc = 0.0001
 learning_rate_gen  = 0.0001
 image_size = 64
 n_samples_w = 5
 n_samples_h = 5
+pnoise_scale = 0.02
 
 def normalize(x):
     return (x - np.min(x)) / (np.max(x) - np.min(x))
@@ -67,11 +70,11 @@ def linear(input_, output_size, name="linear"):
 def generator(z):
     with tf.variable_scope("generator"):
         batch_size = tf.shape(z)[0]
-        l1 = lrelu(linear(z, 4 * 4 * 32))
-        l1 = tf.reshape(l1, [-1, 4, 4, 32])
-        l2 = lrelu(deconv2d(l1, [batch_size, 8, 8, 32], name="deconv_l1"))
-        l3 = lrelu(deconv2d(l2, [batch_size, 16, 16, 16], name="deconv_l2"))
-        l4 = lrelu(deconv2d(l3, [batch_size, 32, 32, 8], name="deconv_l3"))
+        l1 = lrelu(linear(z, 4 * 4 * 64))
+        l1 = tf.reshape(l1, [-1, 4, 4, 64])
+        l2 = lrelu(deconv2d(l1, [batch_size, 8, 8, 64], name="deconv_l1"))
+        l3 = lrelu(deconv2d(l2, [batch_size, 16, 16, 64], name="deconv_l2"))
+        l4 = lrelu(deconv2d(l3, [batch_size, 32, 32, 64], name="deconv_l3"))
         l5 = tf.sigmoid(deconv2d(l4, [batch_size, 64, 64, 1], name="deconv_l4"))
         return l5
 
@@ -79,9 +82,9 @@ def descriminator(x, reuse=False):
     with tf.variable_scope("descriminator") as scope:
         if reuse:
             scope.reuse_variables()
-        l1 = lrelu(conv2d(x, 8, name="conv_l1"))
-        l2 = lrelu(conv2d(l1, 16, name="conv_l2"))
-        l3 = lrelu(conv2d(l2, 32, name="conv_l3"))
+        l1 = lrelu(conv2d(x, 64, name="conv_l1"))
+        l2 = lrelu(conv2d(l1, 64, name="conv_l2"))
+        l3 = lrelu(conv2d(l2, 64, name="conv_l3"))
         l4 = lrelu(conv2d(l3, 64, name="conv_l4"))
         l5 = linear(tf.reshape(l4, [-1, 4 * 4 * 64]), 1)
         return tf.sigmoid(l5), l5, [l1, l2, l3, l4]
@@ -101,7 +104,23 @@ def get_real_data_batch_x(batch_size):
     batch_xs = np.zeros((batch_size, image_size, image_size, 1))
     for i in range(batch_size):
         x = normalize(get_subimage())
+        x = np.rot90(x, random.randint(0, 3))
+        if random.randint(0, 1) == 0:
+            x = np.fliplr(x)
+        if random.randint(0, 1) == 0:
+            x = np.flipud(x)
         batch_xs[i, 0:image_size, 0:image_size, 0] = x
+    return batch_xs
+
+def get_perlin_data_batch_x(batch_size):
+    batch_xs = np.zeros((batch_size, image_size, image_size, 1))
+    for i in range(batch_size):
+        pnoise_offset = random.random()*100000
+        img = np.zeros((image_size, image_size))
+        for x in range(image_size):
+            for y in range(image_size):
+                img[y, x] = pnoise2(x * pnoise_scale + pnoise_offset, y * pnoise_scale + pnoise_offset, 1)
+        batch_xs[i, 0:image_size, 0:image_size, 0] = normalize(img)
     return batch_xs
 
 # def get_real_data_batch_x(batch_size):
@@ -117,10 +136,13 @@ gen = generator(Z)
 
 X = tf.placeholder("float", [None, image_size, image_size, 1])
 real_desc, real_desc_logits, real_desc_filters_out = descriminator(X)
+X2 = tf.placeholder("float", [None, image_size, image_size, 1])
+real_desc2, real_desc_logits2, real_desc_filters_out2 = descriminator(X2, reuse=True)
 
 gen_desc, gen_desc_logits, gen_desc_filters_out = descriminator(gen, reuse=True)
 
 real_desc_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(real_desc_logits, tf.ones_like(real_desc_logits)))
+real_desc_cost2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(real_desc_logits2, tf.zeros_like(real_desc_logits)))
 gen_desc_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(gen_desc_logits, tf.zeros_like(gen_desc_logits)))
 gen_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(gen_desc_logits, tf.ones_like(gen_desc_logits)))
 
@@ -129,6 +151,7 @@ gen_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(gen_desc_logit
 # gen_cost = tf.reduce_mean(tf.pow(tf.ones_like(gen_desc) - gen_desc, 2))
 
 desc_cost = real_desc_cost + gen_desc_cost
+desc_cost2 = real_desc_cost + real_desc_cost2
 
 t_vars = tf.trainable_variables()
 desc_vars = [var for var in t_vars if 'descriminator' in var.name]
@@ -137,6 +160,7 @@ desc_filter_vars = [var for var in desc_vars if 'conv_' in var.name and '/w' in 
 gen_filter_vars = [var for var in gen_vars if 'conv_' in var.name and '/w' in var.name]
 
 desc_optim = tf.train.AdamOptimizer(learning_rate_desc, beta1=0.5).minimize(desc_cost, var_list=desc_vars)
+desc_optim2 = tf.train.AdamOptimizer(0.0001).minimize(desc_cost2, var_list=desc_vars)
 gen_optim = tf.train.AdamOptimizer(learning_rate_gen, beta1=0.5).minimize(gen_cost, var_list=gen_vars)
 
 init = tf.initialize_all_variables()
@@ -157,6 +181,8 @@ filters_plot_f, filters_plot_a = plt.subplots(1, 2)
 filters_plot_f.show()
 filters_plot_a[0].set_label("descriminator")
 filters_plot_a[1].set_label("generator")
+variance_filters_plot_f, variance_filters_plot_a = plt.subplots(2, 1)
+variance_filters_plot_f.show()
 print("Creating plots done")
 
 viz_filters_batch_x = get_real_data_batch_x(1)
@@ -181,7 +207,7 @@ def pack_filters(filters_array, width=256):
         max_row_height = max(max_row_height, filtr.shape[1])
     packed_image = np.zeros((height, width))
     for i in range(len(filters_array)):
-        filtr = normalize(filters_array[i])
+        filtr = filters_array[i]
         x_pos, y_pos = positions[i]
         packed_image[y_pos:(y_pos + filtr.shape[1]), x_pos:(x_pos + filtr.shape[0])] = filtr
     return packed_image
@@ -191,7 +217,7 @@ def filters_out_to_array(filters_out):
         filters = filters_out[y]
         for x in range(filters.shape[-1]):
             img = normalize(np.reshape(filters[0, 0:, 0:, x], (filters.shape[1], filters.shape[2])))
-            filters_array.append(img)
+            filters_array.append(normalize(img))
     return filters_array
 def visualize_filters_out():
     res = sess.run(real_desc_filters_out + gen_desc_filters_out + [gen], feed_dict={ X: viz_filters_batch_x, Z: viz_filters_batch_z })
@@ -213,11 +239,20 @@ def filter_vars_to_array(filter_vars):
         print(filter_data.shape)
         for x in range(filter_data.shape[2]):
             for y in range(filter_data.shape[3]):
-                filters_array.append(filter_data[0:, 0:, x, y])
+                filters_array.append(normalize(filter_data[0:, 0:, x, y]))
+    filters_array.sort(lambda a, b: -1 if np.mean(np.power(a, 2)) < np.mean(np.power(b, 2)) else 1)
     return filters_array
+def filters_array_variation(filters_array):
+    v = 0.0
+    for i in range(len(filters_array) - 1):
+        v += np.mean(np.power((filters_array[i] - filters_array[i + 1]), 2))
+    return v
 def visualize_filters():
-    filters_plot_a[0].imshow(pack_filters(filter_vars_to_array(desc_filter_vars), width=512), interpolation="nearest")
-    filters_plot_a[1].imshow(pack_filters(filter_vars_to_array(gen_filter_vars), width=512), interpolation="nearest")
+    desc_filters_array = filter_vars_to_array(desc_filter_vars)
+    gen_filters_array = filter_vars_to_array(gen_filter_vars)
+    filters_plot_a[0].imshow(pack_filters(desc_filters_array, width=512), interpolation="nearest")
+    filters_plot_a[1].imshow(pack_filters(gen_filters_array, width=512), interpolation="nearest")
+    return filters_array_variation(desc_filters_array), filters_array_variation(gen_filters_array)
 
 print("Plotting real data")
 real_data = get_real_data_batch_x(n_samples_w * n_samples_h)
@@ -229,18 +264,33 @@ print("Plotting real data done")
 sample_z = get_data_batch_z(n_samples_w*n_samples_h)
 
 print("Creating session")
+variance_desc_filters_points = []
+variance_gen_filters_points = []
 with tf.Session() as sess:
     sess.run(init)
 
+
+    # for i in range(1000):
+    #     batch_x1 = get_real_data_batch_x(batch_size)
+    #     batch_x2 = get_perlin_data_batch_x(batch_size)
+    #     _, c1a, c1b = sess.run([desc_optim2, real_desc_cost, real_desc_cost2], feed_dict={ X: batch_x1, X2: batch_x2 })
+    #     print("pretrain", "i", i, "cost_real_desc", c1a, "cost_real_desc2", c1b)
+
     for epoch in range(1000):
         visualize_filters_out()
-        visualize_filters()
+        variance_desc_filters, variance_gen_filters = visualize_filters()
+        variance_desc_filters_points.append(variance_desc_filters)
+        variance_gen_filters_points.append(variance_gen_filters)
+        variance_filters_plot_a[0].plot(variance_desc_filters_points)
+        variance_filters_plot_a[1].plot(variance_gen_filters_points)
+        print("variance_desc_filters", variance_desc_filters, "variance_gen_filters", variance_gen_filters)
         plt.pause(0.05)
 
         for i in range(100):
             batch_x = get_real_data_batch_x(batch_size)
             batch_z = get_data_batch_z(batch_size)
             _, c1a, c1b = sess.run([desc_optim, real_desc_cost, gen_desc_cost], feed_dict={ X: batch_x, Z: batch_z })
+            batch_z = get_data_batch_z(batch_size)
             _, c2 = sess.run([gen_optim, gen_cost], feed_dict={ Z: batch_z })
             costs.append((c1a, c1b, c2))
             print("epoch", epoch, "i", i, "cost_real_desc", c1a, "cost_gen_desc", c1b, "cost_gen", c2)
